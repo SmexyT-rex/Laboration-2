d400e933f0b2c322cda28dd63f91851eb6127b7e
SmexyT-rex <martin.hagelin@hotmail.com>
2025-09-16 12:24:08 +0200

    styling and polishing responsiveness

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+const navigationBar = document.querySelector("nav");
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
@@ -9,8 +11,10 @@ hamMenu.addEventListener("click", () => {
   if (offScreenMenu.classList.contains("active")) {
     document.body.style.overflow = "hidden";
     offScreenMenu.style.overflow = "visible";
+    navigationBar.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
   } else {
     document.body.style.overflow = "";
     offScreenMenu.style.overflow = "visible";
+    navigationBar.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
   }
 });
