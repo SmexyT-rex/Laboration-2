0eb4d81528ff12ddf601176ef13834791d079566
SmexyT-rex <martin.hagelin@hotmail.com>
2025-09-16 10:35:45 +0200

    fixed the responsive hamburger bar menu

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const offScreenMenu = document.querySelector(".off-screen-menu");
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+
+  if (offScreenMenu.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+    offScreenMenu.style.overflow = "visible";
+  } else {
+    document.body.style.overflow = "";
+    offScreenMenu.style.overflow = "visible";
+  }
 });
